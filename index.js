61241f98b699caa6ab30fc44a8bed9daf1d904f0
Mark Teekman <mark.teekman@gmail.com>
2026-01-15 19:33:20 +0100

    feat: add index.js to export launcher components and styles

---
+++ index.js
@@ -0,0 +1,9 @@
+// Import shared styles
+import './src/styles/index.css'
+
+// Export components
+export { default as Launcher } from './src/components/launcher/Launcher.astro'
+export { default as LauncherTrigger } from './src/components/launcher/LauncherTrigger.astro'
+export { default as LauncherList } from './src/components/launcher/LauncherList.astro'
+export { default as LauncherItem } from './src/components/launcher/LauncherItem.astro'
+export { default as LauncherGroup } from './src/components/launcher/LauncherGroup.astro'
